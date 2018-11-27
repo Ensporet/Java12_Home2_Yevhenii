d16aa12710ada141911d79b3d10ad570e7fe2919
Ensporet <43299446+Ensporet@users.noreply.github.com>
2018-11-27 20:29:53 +0200

    V01
    new - Создает панель
    new - Выводит панель в центр экрана
    
    Co-Authored-By: ensporet <ensporet@users.noreply.github.com>

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import SC_Librari.Window.TestFrame;
+import SC_Librari.Window.W_Default;
+import SC_Librari.Window.W_КонольВвода4Показателей;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+
+                new W_КонольВвода4Показателей();
+
+            }
+        });
+
+
+
+
+    }
+}
