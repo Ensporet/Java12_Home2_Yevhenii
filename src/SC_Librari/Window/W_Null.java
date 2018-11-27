690d86ab961fee00ffdf0bd9e8d37e9d57c330a4
Ensporet <43299446+Ensporet@users.noreply.github.com>
2018-11-27 20:40:28 +0200

    Revert "Revert "V01""
    This reverts commit 5249d9a8efdaf70bc3f84731e7a20344cee82ce0.

---
+++ src/SC_Librari/Window/W_Null.java
@@ -0,0 +1,8 @@
+package SC_Librari.Window;
+
+public interface W_Null {
+
+W_Default d = new W_Default();
+
+
+}
---
+++ src/SC_Librari/Window/W_КонольВвода4Показателей.java
@@ -0,0 +1,14 @@
+package SC_Librari.Window;
+
+public class W_КонольВвода4Показателей extends W_Default {
+
+public  W_КонольВвода4Показателей(){
+
+
+
+this.MovingWindow(W_Default.CENTER);
+
+}
+
+
+}
